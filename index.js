8cef0d53d16f33b1d5fcc0b3e0d205a9a6ec32f6
Nic Peterson <nic@peterson.to>
2022-01-16 16:41:51 -0800

    Finished updating the js and css to handle apending X grids via for loop and flex wrap

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const screen = document.querySelector(".screen");
-const grid = document.createElement("div");
-grid.setAttribute("class", "grid");
-console.log(`screen: ${screen}\ngrid: ${grid}`);
-screen.appendChild(grid);
 
-
+for (let i = 0; i < 16; i++) {
+  const grid = document.createElement("div");
+  grid.setAttribute("class", "grid");
+  //console.log(`screen: ${screen}\ngrid: ${grid}`);
+  screen.appendChild(grid);
+}
