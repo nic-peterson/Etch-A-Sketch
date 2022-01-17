0549e35df025edf9403106d5ca563dad90f3bdac
Nic Peterson <nic@peterson.to>
2022-01-17 06:29:16 -0800

    Completed the primary requirements. Going to look at adding the optional functionality.

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const screen = document.querySelector(".screen");
+const clearBtn = document.querySelector("#clear");
 
-for (let i = 0; i < 16; i++) {
-  const side = 125;
-  const grid = document.createElement("div");
-  grid.setAttribute("style", `height:${side}px;width:${side}px;`)
-  grid.setAttribute("class", "grid");
-  //grid.setAttribute("height", 125px);
-  //grid.setAttribute("width", 125px);
-  //console.log(`screen: ${screen}\ngrid: ${grid}`);
-  screen.appendChild(grid);
-}
+const setScreen = (side) => {
+  const s = 480 / side;
+  const square = side * side;
+  for (let i = 0; i < square; i++) {
+    const grid = document.createElement("div");
+    grid.setAttribute("style", `height:${s}px;width:${s}px;`);
+    grid.setAttribute("class", "grid");
+    grid.addEventListener(
+      "mouseover",
+      (e) => {
+        e.target.style.backgroundColor = "black";
+      },
+      false
+    );
+    screen.appendChild(grid);
+  }
+};
+
+const changeGridCount = () => {
+  let side;
+  const userSide = prompt(
+    "Choose x for the number of x by x grids you would like!"
+  );
+  console.log(
+    `typeof userSide: ${typeof userSide}\nuserSide: ${userSide}\nparseInt(userSide): ${parseInt(
+      userSide
+    )}\ntypeof parseInt(userSide): ${typeof parseInt(userSide)}`
+  );
+  if (parseInt(userSide) < 0 || parseInt(userSide) > 100) {
+    side = 16;
+  } else {
+    side = userSide;
+  }
+  console.log(`side: ${side}`);
+
+  setScreen(side);
+};
+
+const chooseColor = () => {
+  return;
+};
+
+setScreen(16);
+
+clearBtn.addEventListener("click", () => {
+  document
+    .querySelectorAll(".grid")
+    .forEach((e) => e.parentNode.removeChild(e));
+  changeGridCount();
+});
