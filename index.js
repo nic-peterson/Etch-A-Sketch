8b9940d6bd32f1883d0afa45a29b0279513d366c
Nic Peterson <nic@peterson.to>
2022-01-16 16:46:19 -0800

    Added a variable, side, that can be dynamically attributed to .div grid height and width

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const screen = document.querySelector(".screen");
 
 for (let i = 0; i < 16; i++) {
+  const side = 125;
   const grid = document.createElement("div");
+  grid.setAttribute("style", `height:${side}px;width:${side}px;`)
   grid.setAttribute("class", "grid");
+  //grid.setAttribute("height", 125px);
+  //grid.setAttribute("width", 125px);
   //console.log(`screen: ${screen}\ngrid: ${grid}`);
   screen.appendChild(grid);
 }
