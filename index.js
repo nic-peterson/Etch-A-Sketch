e2443437ead6c6f503f85c34b8b4340d6648cac2
Nic Peterson <nic@peterson.to>
2022-01-16 16:39:40 -0800

    Setup the html elements and created a single div inside the screen.

---
+++ index.js
@@ -0,0 +1,7 @@
+const screen = document.querySelector(".screen");
+const grid = document.createElement("div");
+grid.setAttribute("class", "grid");
+console.log(`screen: ${screen}\ngrid: ${grid}`);
+screen.appendChild(grid);
+
+
